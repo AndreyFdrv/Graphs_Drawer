1c147a32f1a7d4a7726df7a9247f4af0f0832435
AndreyFdrv <andrei_hedorov@mail.ru>
2017-06-07 07:24:12 -0700

    Функция добавления точки на виджет

--- GraphsWidget.cpp
+++ GraphsWidget.cpp
@@ -6,17 +6,107 @@ GraphsWidget::GraphsWidget(QWidget *parent) :
     QWidget(parent)
 {
     legend_area_width=200;
+    x_offset=50;
+    y_offset=50;
+    x_divisions_count=10;
+    y_divisions_count=10;
+    x_name="x";
+    y_name="y";
 }
 GraphsWidget::~GraphsWidget()
 {
     delete painter;
+}
+void GraphsWidget::paintEvent(QPaintEvent *event)
+{
+    Q_UNUSED(event);
+    painter=new QPainter(this);
+    Clear();
+    if(graphs.length()!=0)
+    {
+        ComputeBorders();
+        ComputeScales();
+        DrawGrid();
+        DrawGraphs();
+        DrawAxises();
+    }
+}
+void GraphsWidget::AddPoint(QString graph_name, double x, double y)
+{
+    Graph *graph=NULL;
+    for(int i=0; i<graphs.length(); i++)
+    {
+        if(graphs.at(i)->getName()==graph_name)
+        {
+            graph=graphs.at(i);
+            break;
+        }
+    }
+    if(graph==NULL)
+    {
+        QMessageBox msgBox;
+        QString message="Графика с именем \""+graph_name+
+                "\" не существует\n";
+        msgBox.setText(message);
+        msgBox.exec();
+        return;
+    }
+    graph->AddPoint(x, y);
+    repaint();
+}
+void GraphsWidget::DrawAxises()
+{
+    int x=width()-legend_area_width;
+    int y=height()-y_offset;
+    DrawLine(x-10, y-10, x, y, QColor(Qt::black));
+    DrawLine(x-10, y+10, x, y, QColor(Qt::black));
+    x=x_offset;
+    y=0;
+    DrawLine(x-10, y+10, x, y, QColor(Qt::black));
+    DrawLine(x+10, y+10, x, y, QColor(Qt::black));
+    double x_steep=(x_max-x_min)/(x_divisions_count-1);
+    for (double i = x_min; i < x_max; i+=x_steep)
+    {
+        x=x_scale * (i - x_min)+x_offset;
+        y=height()-y_offset;
+        DrawLine(x, y-5, x, y+5, QColor(Qt::black));
+        QLabel *label=new QLabel(this);
+        x_labels.insert(x_labels.size(), label);
+        label->setText(QString::number(i, 'f', 2));
+        label->move(x-15, y+3);
+        label->show();
+    }
+    double y_steep=(y_max-y_min)/(y_divisions_count-1);
+    for(double i=y_min; i<y_max; i+=y_steep)
+    {
+        x=x_offset;
+        y=height()-y_offset-y_scale*(i-y_min);
+        DrawLine(x-5, y, x+5, y, QColor(Qt::black));
+        QLabel *label=new QLabel(this);
+        y_labels.insert(y_labels.size(), label);
+        label->setText(QString::number(i, 'f', 2));
+        label->move(x-45, y-7);
+        label->show();
+    }
+    x_name_label=new QLabel(this);
+    x_name_label->setText(x_name);
+    x=width()-legend_area_width;
+    y=height()-y_offset;
+    x_name_label->move(x, y);
+    x_name_label->show();
+    y_name_label=new QLabel(this);
+    y_name_label->setText(y_name);
+    x=x_offset;
+    y=0;
+    y_name_label->move(x+3, y);
+    y_name_label->show();
 }
 void GraphsWidget::Clear()
 {
     painter->setPen(QPen(Qt::black, 1, Qt::SolidLine, Qt::FlatCap));
     painter->setBrush(QBrush(Qt::white, Qt::SolidPattern));
     painter->drawRect(0, 0, this->width()-1, this->height()-1);
-    painter->drawRect(0, 0, this->width()-legend_area_width, this->height()-1);
+    painter->drawRect(x_offset, 0, this->width()-legend_area_width-x_offset, this->height()-y_offset);
 }
 void GraphsWidget::ComputeBorders()
 {
@@ -50,11 +140,11 @@ void GraphsWidget::ComputeScales()
     if(x_max==x_min)
         x_scale=1;
     else
-        x_scale=(width()-legend_area_width)/(x_max-x_min);
+        x_scale=(width()-legend_area_width-x_offset)/(x_max-x_min);
     if(y_max==y_min)
         y_scale=1;
     else
-        y_scale=height()/(y_max-y_min);
+        y_scale=(height()-y_offset)/(y_max-y_min);
 }
 void GraphsWidget::DrawPoint(int x, int y, QColor color)
 {
@@ -104,16 +194,16 @@ void GraphsWidget::DrawGraphs()
         if(graphs.at(i)->PointsCount()==0)
             break;
         double x0=graphs.at(i)->getX(0);
-        x0=x_scale*(x0-x_min);
+        x0=x_scale*(x0-x_min)+x_offset;
         double y0=graphs.at(i)->getY(0);
-        y0=height()-y_scale*(y0-y_min);
+        y0=height()-y_offset-y_scale*(y0-y_min);
         DrawPoint((int)x0, (int)y0, color);
         for(int j=1; j<graphs.at(i)->PointsCount(); j++)
         {
             double x1=graphs.at(i)->getX(j);
-            x1=x_scale*(x1-x_min);
+            x1=x_scale*(x1-x_min)+x_offset;
             double y1=graphs.at(i)->getY(j);
-            y1=height()-y_scale*(y1-y_min);
+            y1=height()-y_offset-y_scale*(y1-y_min);
             DrawLine((int)x0, (int)y0, (int)x1, (int)y1, color);
             DrawPoint((int)x1, (int)y1, color);
             x0=x1;
@@ -121,14 +211,20 @@ void GraphsWidget::DrawGraphs()
         }
     }
 }
-void GraphsWidget::paintEvent(QPaintEvent *event)
+void GraphsWidget::DrawGrid()
 {
-    Q_UNUSED(event);
-    painter=new QPainter(this);
-    Clear();
-    ComputeBorders();
-    ComputeScales();
-    DrawGraphs();
+    double x_steep=(x_max-x_min)/(x_divisions_count-1);
+    for (double i = x_min+x_steep; i < x_max; i+=x_steep)
+    {
+        int x = x_scale * (i - x_min)+x_offset;
+        DrawLine(x, height()-y_offset, x, 0, QColor(Qt::gray));
+    }
+    double y_steep=(y_max-y_min)/(y_divisions_count-1);
+    for(double i=y_min+y_steep; i<y_max; i+=y_steep)
+    {
+        int y=height()-y_offset-y_scale*(i-y_min);
+        DrawLine(x_offset, y, width()-legend_area_width, y, QColor(Qt::gray));
+    }
 }
 void GraphsWidget::AddGraph(char *filename)
 {
--- GraphsWidget.h
+++ GraphsWidget.h
@@ -5,14 +5,18 @@
 #include <QVector>
 #include "Graph.h"
 #include <QPainter>
+#include <QLabel>
+#include <QString>
 class GraphsWidget : public QWidget
 {
     Q_OBJECT
 private:
     int legend_area_width;
+    int x_offset, y_offset;
     QVector<Graph *> graphs;//точки отсортированы по координате x
     QPainter *painter;
     double x_min, x_max, y_min, y_max, x_scale, y_scale;
+    int x_divisions_count, y_divisions_count;
     void Clear();
     void DrawGraph(Graph *graph);
     void ComputeBorders();
@@ -21,11 +25,18 @@ private:
     void DrawPoint(int x, int y, QColor color);
     QColor GenerateColor(int index);
     void DrawLine(int x0, int y0, int x1, int y1, QColor color);
+    void DrawGrid();
+    void DrawAxises();
+    QVector<QLabel *> x_labels;
+    QVector<QLabel *> y_labels;
+    QString x_name, y_name;
+    QLabel *x_name_label, *y_name_label;
 public:
     explicit GraphsWidget(QWidget *parent = 0);
     ~GraphsWidget();
     void AddGraph(char *filename);
     bool isGraphExsist(QString name);
+    void AddPoint(QString graph_name, double x, double y);
 protected:
     void paintEvent(QPaintEvent *event);
 signals:
